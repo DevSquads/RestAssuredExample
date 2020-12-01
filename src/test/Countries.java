08db9665b63c46a71c66d081669082b5bcd119d9
A.Yasser <ahmed.yasser.asu@gmail.com>
2020-12-01 16:39:48 +0200

    restAssured Example not using maven

---
+++ src/test/Countries.java
@@ -0,0 +1,39 @@
+package test;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Countries {
+
+    private String API_URL = "https://restcountries-v1.p.rapidapi.com/";
+    private String API_HOST = "restcountries-v1.p.rapidapi.com";
+    private String API_KEY = "21e6f4f25dmsh8bbed3c079a43acp14bd7bjsnb34a3bcca5f8";
+
+    @Test
+    public void getByName() {
+        String countryName = "norge";
+        String endPointURL = String.format("%s%s",this.API_URL,"name");
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("X-RapidAPI-Host", this.API_HOST)
+                .header("X-RapidAPI-Key", this.API_KEY)
+                .when()
+                .get(String.format("%s/%s",endPointURL,countryName))
+                .then()
+                .statusCode(200)
+                .extract().response();
+        JsonPath response_json = response.body().jsonPath();
+        assertEquals(getValueOf(response_json, "name"), "Norway");
+        assertEquals(getValueOf(response_json, "region"), "Europe");
+    }
+
+    private String getValueOf(JsonPath response_json, String key) {
+        //      note jsonPath returns an array , that's why we used name[0] => check https://goessner.net/articles/JsonPath/
+        return response_json.getString(String.format("%s[0]", key));
+    }
+}
